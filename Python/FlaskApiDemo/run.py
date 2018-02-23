45b9d13918bd60a16480bb036823a243fa045b0b
Lucas <lucascow@users.noreply.github.com>
2018-02-23 17:05:13 +0800

    Create run.py

---
+++ Python/FlaskApiDemo/run.py
@@ -0,0 +1,15 @@
+# This is simple demo to build restful api for the small scale project, with Python & Flask
+#
+# (c) Lucas YU <lucasyu8.developer@gmail.com>
+#
+# 1. First of all install Flask:
+#		pip3 install Flask 	
+#
+# 2. Run the app (in python 3):
+# 		FLASK_APP=run.py python3 -m flask run
+#
+# Debug(Hot Deploy):
+# 		FLASK_APP=run.py FLASK_DEBUG=1 python3 -m flask run
+
+
+from config.flaskApp import app
